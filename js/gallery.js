4aa06b4b60ef5343580aa89add37b62764652c64
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-07-04 02:52:23 +0530

    Revert "First Website Upload"
    This reverts commit a1212258ca6714506242e04915ce8766502a8267.

--- js/gallery.js
+++
@@ -1,5 +0,0 @@
-function upDate(PreviewPic)
-	{
-	document.getElementById('bigDisplay').style.backgroundImage = "url("+ PreviewPic.src +")";
-	document.getElementById('bigDisplay').style.transition = "1.5s";
-	}
