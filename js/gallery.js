9adafac1446e5befdd3132666e55a4b8e49e0c30
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-07-05 00:25:01 +0530

    Second Website Update

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,5 @@
-function upDate(PreviewPic)
-	{
-	document.getElementById('bigDisplay').style.backgroundImage = "url("+ PreviewPic.src +")";
-	document.getElementById('bigDisplay').style.transition = "1.5s";
-	}
+function myFunction(imgs) {
+	var expandImg = document.getElementById("expandedImg");
+	expandImg.src = imgs.src;
+	expandImg.parentElement.style.display = "block";
+}
