fa6c7c3eac0e53101ba44d45b755afd201a79da3
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-07-12 12:54:12 +0530

    Update

---
+++ projects.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang = "en">
+
+<head>
+	<meta charset = "UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Kedar's Projects</title>
+
+	<link rel = "stylesheet" href = "css/bootstrap.css">
+	<link rel = "stylesheet" href = "css/navbar_styling.css">
+	<link rel = "stylesheet" href = "css/projects.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300i,400" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse">
+  
+	<div class="navbar-header">
+	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+	</button>
+	<a class="navbar-brand" href="index.html">Kedar Wadhavkar</a>
+	</div>
+
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+
+        <li class="adjustPadding"><a href="aboutMe.html" target = "_blank">About Me</a></li>
+
+	<li role="presentation" class="dropdown adjustPadding active">
+	<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+	Career<span class="caret"></span></a>
+	<ul class="dropdown-menu">
+		<li class = "active2"> <a href ="projects.html" target = "_blank">Projects</a>
+		<li> <a href ="thesis.html" target = "_blank">Thesis</a>
+		<li> <a href ="skills.html" target = "_blank">Skills</a>
+	</ul>
+  	</li>
+
+        <li class="adjustPadding"><a href="supra.html" target = "_blank">SUPRA</a></li>
+ 
+	<li role="presentation" class="dropdown adjustPadding">
+	<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+	Personal Info<span class="caret"></span></a>
+	<ul class="dropdown-menu">
+		<li> <a href ="achievements.html" target = "_blank">Achievements</a>
+		<li> <a href ="hobbies.html" target = "_blank">Hobbies</a>
+		<li> <a href ="gallery.html" target = "_blank">Gallery</a>
+	</ul>
+  	</li>
+        
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right adjustPadding">
+        <li><a href="#">Contact Me</a></li>
+      </ul>
+    </div>
+
+</nav>
+
+<div class = "col-xs-12 col-sm-12 col-md-12 col-lg-12 container">
+
+	<div id="SUPRA" class="content">
+	<div class = "disp">
+
+  		<div class="card s1" id = "one">
+    			<h3 class="title">Card 1</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+      				<div class="filledbar"></div>
+  			</div>
+		</div>
+
+  		<div class="card s2" id = "two">
+			<h3 class="title">Card 2</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+    		</div>
+
+		<div class="card s3" id = "three">
+			<h3 class="title">Card 3</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+		</div>
+
+		<div class="card s4" id = "four">
+			<h3 class="title">Card 4</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+    		</div>
+
+	</div>
+	</div>
+	
+
+	<div id="DELERIOS" class="content">
+	<div class = "disp">
+
+  		<div class="card d1" id = "one">
+    			<h3 class="title">Card 1</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+      				<div class="filledbar"></div>
+  			</div>
+		</div>
+
+  		<div class="card d2" id = "two">
+			<h3 class="title">Card 2</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+    		</div>
+
+		<div class="card d3" id = "three">
+			<h3 class="title">Card 3</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+		</div>
+
+		<div class="card d4" id = "four">
+			<h3 class="title">Card 4</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+    		</div>
+	</div>
+	</div>
+
+	<div id="Website" class="content">
+	<div class = "disp">
+
+  		<div class="card w1" id = "one">
+    			<h3 class="title">Card 1</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+      				<div class="filledbar"></div>
+  			</div>
+		</div>
+
+  		<div class="card w2" id = "two">
+			<h3 class="title">Card 2</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+    		</div>
+
+		<div class="card w3" id = "three">
+			<h3 class="title">Card 3</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+		</div>
+
+		<div class="card w4" id = "four">
+			<h3 class="title">Card 4</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+    		</div>
+	</div>
+	</div>
+	
+	<div id="Internship" class="content">
+	<div class = "disp">
+
+  		<div class="card i1" id = "one">
+    			<h3 class="title">Card 1</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+      				<div class="filledbar"></div>
+  			</div>
+		</div>
+
+  		<div class="card i2" id = "two">
+			<h3 class="title">Card 2</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+    		</div>
+
+		<div class="card i3" id = "three">
+			<h3 class="title">Card 3</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+		</div>
+
+		<div class="card i4" id = "four">
+			<h3 class="title">Card 4</h3>
+			<div class="bar">
+				<div class="emptybar"></div>
+				<div class="filledbar"></div>
+			</div>
+    		</div>
+	</div>
+	</div>
+
+	<button class="tablink but1" onclick="openProject('SUPRA', this, '#232323')" id="defaultOpen">SUPRA</button>
+	<button class="tablink but2" onclick="openProject('DELERIOS', this, '#232323')">DELERIOS</button>
+	<button class="tablink but3" onclick="openProject('Website', this, '#232323')">Website</button>
+	<button class="tablink but4" onclick="openProject('Internship', this, '#232323')">Internship</button>
+
+</div>
+
+
+<div class = "col-xs-12 col-sm-12 col-md-12 col-lg-12 buttoncontainer">
+
+	<div class = "col-xs-6 col-sm-3 col-md-3 col-lg-3 maindiv"><div class = "buttoneffect"><a href = "#"><span class = "effect"><i class="fa fa-flag-checkered fa-2x" aria-hidden="true"><br><h3 class = "projectName">SUPRA</h3></i></span><a></div></div>
+
+	<div class = "col-xs-6 col-sm-3 col-md-3 col-lg-3 maindiv"><div class = "buttoneffect"><a href = "#"><span class = "effect"><i class="fa fa-cubes fa-2x" aria-hidden="true"><br><h3 class = "projectName">DELERIO</h3></i></span><a></div></div>
+
+	<div class = "col-xs-6 col-sm-3 col-md-3 col-lg-3 maindiv"><div class = "buttoneffect"><a href = "#"><span class = "effect"><i class="fa fa-laptop fa-2x" aria-hidden="true"><br><h3 class = "projectName">Website</h3></i></span><a></div></div>
+
+	<div class = "col-xs-6 col-sm-3 col-md-3 col-lg-3 maindiv"><div class = "buttoneffect"><a href = "#"><span class = "effect"><i class="fa fa-suitcase fa-2x" aria-hidden="true"><br><h3 class = "projectName">Internship</h3></i></span><a></div></div>
+
+
+</div>
+
+
+	<!--<a href = "#" class = "col-xs-6 col-sm-3 col-md-3 col-lg-3 buttoneffect"><span class = "effect"><i class="fa fa-flag-checkered fa-2x" aria-hidden="true"><br><h3 class = "projectName">SUPRA</h3></i></span><a>  -->
+
+
+
+<form id = "contact-form" method = "post" action="contactform.php">
+	<fieldset>
+	<legend>Personal Information</legend>
+	<label for = 'name'>Name</label>
+	<input type = 'text' id = 'name' name = 'name'>
+	<br><hr>
+	<label>Email: <input type = 'email' name = 'email' placeholder="me@me.com"></label>
+	<br><hr>
+	<label for = 'birth'>Birthdate</label>
+	<input type = 'date' id = 'birth' value = 'birth'>
+	<br><hr>
+	<input type = 'submit' name = 'submit' value = 'Submit'>
+	</fieldset>
+</form>
+
+
+
+
+
+
+
+
+
+
+<script>
+
+function openProject(projectName,elmnt,color) {
+
+	var i, content, tablinks;
+
+	content = document.getElementsByClassName("content");
+
+	for (i = 0; i < content.length; i++) {
+	content[i].style.display = "none";
+	}
+
+	tablinks = document.getElementsByClassName("tablink");
+
+	for (i = 0; i < tablinks.length; i++) {
+	tablinks[i].style.backgroundColor = "";
+	}
+
+	document.getElementById(projectName).style.display = "block";
+	elmnt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
+
+</script>
+
+
+	<!-- scripts required in this program i placed here -->
+	
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script  type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+	<!--finished-->
+
+</body>
+
+</html>
+
+
+
+
+
