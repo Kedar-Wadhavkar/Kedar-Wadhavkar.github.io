c270d1489e72810c07bf7ece560dd59cc19cdc10
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-07-21 02:05:49 +0530

    Update

--- js/app.js
+++ js/app.js
@@ -59,30 +59,14 @@ window.onload = function() {
 
 
 
-//Get the button
-var mybutton = document.getElementById("myBtn");
+var mybutton = document.querySelector('.gotopbtn');
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-function titleVisible(name) {
-    document.getElementById(name).style.visibility = "visible";
-  }
-  function titleHidden(name) {
-    document.getElementById(name).style.visibility = "hidden";
-  
-  }
