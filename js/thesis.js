4f0c374c98a042bd0cce310253504c9e846c7455
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-07-25 00:14:24 +0530

    Update

--- js/thesis.js
+++ js/thesis.js
@@ -1,1 +1,10 @@
+function skillRoute() {
+    let elmnt = document.getElementById("abstractread1");
+    elmnt.scrollIntoView({behavior: 'smooth'});
+  }
+
+function skillRoute2() {
+    let elmnt = document.getElementById("abstractread2");
+    elmnt.scrollIntoView({behavior: 'smooth'});
+  }
 
