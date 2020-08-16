49f27063799980290fcf84ddcf44dcebdca747f9
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-08-16 18:43:58 +0530

    Update

--- js/skills.js
+++ js/skills.js
@@ -1,4 +1,21 @@
 
+var mybutton = document.querySelector('.gotopbtn');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	mybutton.style.display = "block";
+	} 
+
+	else {
+	mybutton.style.display = "none";
+	}
+}
+
+
+
 var Dial = function(container) {
     this.container = container;
     this.size = this.container.dataset.size;
