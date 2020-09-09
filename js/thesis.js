ff51ab69c8c8a2d8b0e5b93ddd8800dc0b3bc9c2
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-09-09 16:42:47 +0530

    Update

--- js/thesis.js
+++ js/thesis.js
@@ -1,3 +1,22 @@
+var mybutton = document.querySelector('.gotopbtn');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	mybutton.style.display = "block";
+	} 
+
+	else {
+	mybutton.style.display = "none";
+	}
+}
+
+
+
+
+
 function thesisRoute() {
     let elmnt = document.getElementById("abstractread1");
     elmnt.scrollIntoView({behavior: 'smooth'});
@@ -7,4 +26,7 @@ function thesisRoute2() {
     let elmnt = document.getElementById("abstractread2");
     elmnt.scrollIntoView({behavior: 'smooth'});
   }
+
+
+
 
