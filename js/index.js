65f0f8c080bedfea685382e32ad3d8db1d151671
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-09-10 00:06:19 +0530

    Update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+var mybutton = document.querySelector('.gotopbtn');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
 
 function upDate(previewPic)
 	{
