65ed28cfd80a6497b35941a68e8ff3bccd8676b3
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-10-07 17:35:56 +0530

    Update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-var mybutton = document.querySelector('.gotopbtn');
+var Button = document.querySelector('.gotopbtn');
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    mybutton.style.display = "block";
+    Button.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    Button.style.display = "none";
   }
 }
 
