d2c0bfd8a353cf66b2ac2c1d87ff98c4cbe4745d
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-10-21 13:19:56 +0530

    Update

--- js/capstone.js
+++ js/capstone.js
@@ -9,3 +9,9 @@ $(".car2").owlCarousel(
 		autoplayTimeout: 3000, autoplayHoverPause: true,
 		responsive: { 0:{items:1, nav: false}, 576:{items:2, nav: false}, 991:{items:3, nav: false}
 	}});
+
+$(".car3").owlCarousel(
+	{	margin: 20, loop: true, autoplay: true,
+		autoplayTimeout: 3000, autoplayHoverPause: true,
+		responsive: { 0:{items:1, nav: false}, 576:{items:2, nav: false}, 991:{items:2, nav: false}
+	}});
