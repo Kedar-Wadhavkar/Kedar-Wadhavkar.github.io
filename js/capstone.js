6f48b3960a3c9dd85d887c6244d6e698532ba5b5
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-10-21 16:22:35 +0530

    Update

--- js/capstone.js
+++ js/capstone.js
@@ -1,17 +1,17 @@
 $(".car1").owlCarousel(
-	{	margin: 20, loop: true, autoplay: true,
-		autoplayTimeout: 3000, autoplayHoverPause: true,
+	{	margin: 0, loop: true, autoplay: true,
+		autoplayTimeout: 10000, autoplayHoverPause: true,
 		responsive: { 0:{items:1, nav: false}, 576:{items:2, nav: false}, 991:{items:3, nav: false}
 	}});
 
 $(".car2").owlCarousel(
-	{	margin: 20, loop: true, autoplay: true,
-		autoplayTimeout: 3000, autoplayHoverPause: true,
-		responsive: { 0:{items:1, nav: false}, 576:{items:2, nav: false}, 991:{items:3, nav: false}
+	{	margin: 10, loop: true, autoplay: true,
+		autoplayTimeout: 10000, autoplayHoverPause: true,
+		responsive: { 0:{items:1, nav: false}, 576:{items:2, nav: false}, 991:{items:2, nav: false}
 	}});
 
 $(".car3").owlCarousel(
-	{	margin: 20, loop: true, autoplay: true,
-		autoplayTimeout: 3000, autoplayHoverPause: true,
-		responsive: { 0:{items:1, nav: false}, 576:{items:2, nav: false}, 991:{items:2, nav: false}
+	{	margin: 0, loop: true, autoplay: true,
+		autoplayTimeout: 10000, autoplayHoverPause: true,
+		responsive: { 0:{items:1, nav: false}, 576:{items:2, nav: false}, 991:{items:3, nav: false}
 	}});
