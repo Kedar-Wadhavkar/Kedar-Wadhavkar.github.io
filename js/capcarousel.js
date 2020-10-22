fd52cac0a8174fab10e11179671d64ab2adf8488
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-10-22 16:26:20 +0530

    Update

--- js/capcarousel.js
+++ js/capcarousel.js
@@ -9,11 +9,11 @@ var img13 = document.getElementById("img13");
 var img14 = document.getElementById("img14");
 var img15 = document.getElementById("img15");
 
-img11.onclick = function(){modal1.style.display = "block";  modalImg1.src = this.src;  captionText.innerHTML = this.alt;}
-img12.onclick = function(){modal1.style.display = "block";  modalImg1.src = this.src;  captionText.innerHTML = this.alt;}
-img13.onclick = function(){modal1.style.display = "block";  modalImg1.src = this.src;  captionText.innerHTML = this.alt;}
-img14.onclick = function(){modal1.style.display = "block";  modalImg1.src = this.src;  captionText.innerHTML = this.alt;}
-img15.onclick = function(){modal1.style.display = "block";  modalImg1.src = this.src;  captionText.innerHTML = this.alt;}
+img11.onclick = function(){modal1.style.display = "block";  modalImg1.src = 'css/art.png';  captionText.innerHTML = this.alt;}
+img12.onclick = function(){modal1.style.display = "block";  modalImg1.src = 'css/cooking.png';  captionText.innerHTML = this.alt;}
+img13.onclick = function(){modal1.style.display = "block";  modalImg1.src = 'css/drawing.png';  captionText.innerHTML = this.alt;}
+img14.onclick = function(){modal1.style.display = "block";  modalImg1.src = 'css/photography.png';  captionText.innerHTML = this.alt;}
+img15.onclick = function(){modal1.style.display = "block";  modalImg1.src = 'css/writing.png';  captionText.innerHTML = this.alt;}
 
 
 
@@ -27,10 +27,10 @@ var img22 = document.getElementById("img22");
 var img23 = document.getElementById("img23");
 var img24 = document.getElementById("img24");
 
-img21.onclick = function(){modal2.style.display = "block";  modalImg2.src = this.src;  captionText.innerHTML = this.alt;}
-img22.onclick = function(){modal2.style.display = "block";  modalImg2.src = this.src;  captionText.innerHTML = this.alt;}
-img23.onclick = function(){modal2.style.display = "block";  modalImg2.src = this.src;  captionText.innerHTML = this.alt;}
-img24.onclick = function(){modal2.style.display = "block";  modalImg2.src = this.src;  captionText.innerHTML = this.alt;}
+img21.onclick = function(){modal2.style.display = "block";  modalImg2.src = 'css/cad.png';  captionText.innerHTML = this.alt;}
+img22.onclick = function(){modal2.style.display = "block";  modalImg2.src = 'css/cae.png';  captionText.innerHTML = this.alt;}
+img23.onclick = function(){modal2.style.display = "block";  modalImg2.src = 'css/crea.png';  captionText.innerHTML = this.alt;}
+img24.onclick = function(){modal2.style.display = "block";  modalImg2.src = 'css/gdnt.png';  captionText.innerHTML = this.alt;}
 
 
 
--- js/capstone.js
+++ js/capstone.js
@@ -1,7 +1,7 @@
 $(".car1").owlCarousel(
 	{	margin: 0, loop: true, autoplay: true,
 		autoplayTimeout: 10000, autoplayHoverPause: true,
-		responsive: { 0:{items:1, nav: false}, 576:{items:2, nav: false}, 991:{items:3, nav: false}
+		responsive: { 0:{items:2, nav: false}, 576:{items:4, nav: false}, 991:{items:6, nav: false}
 	}});
 
 $(".car2").owlCarousel(
