d91228e6e994ad6235d90b1e78cf66cc7a8403c4
Kedar Wadhavkar <kedar.wadhavkar@github.com>
2020-12-09 22:01:30 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,47 +30,47 @@ function Route1() {
 setTimeout(function () {
     let elmnt = document.getElementById("route1");
     elmnt.scrollIntoView('true', {behavior: 'smooth'});
-  }, 600);
+  }, 800);
 }
 
 function Route2() {
 setTimeout(function () {
     let elmnt = document.getElementById("route2");
     elmnt.scrollIntoView('true', {behavior: 'smooth'});
-  }, 600);
+  }, 800);
 }
 
 function Route3() {
 setTimeout(function () {
     let elmnt = document.getElementById("route3");
     elmnt.scrollIntoView('true', {behavior: 'smooth'});
-  }, 600);
+  }, 800);
 }
 
 function Route4() {
 setTimeout(function () {
     let elmnt = document.getElementById("route4");
     elmnt.scrollIntoView('true', {behavior: 'smooth'});
-  }, 600);
+  }, 800);
 }
 
 function Route5() {
 setTimeout(function () {
     let elmnt = document.getElementById("route5");
     elmnt.scrollIntoView('true', {behavior: 'smooth'});
-  }, 600);
+  }, 800);
 }
 
 function Route6() {
 setTimeout(function () {
     let elmnt = document.getElementById("route6");
     elmnt.scrollIntoView('true', {behavior: 'smooth'});
-  }, 600);
+  }, 800);
 }
 
 function Route7() {
 setTimeout(function () {
     let elmnt = document.getElementById("route7");
     elmnt.scrollIntoView('true', {behavior: 'smooth'});
-  }, 600);
+  }, 800);
 }
